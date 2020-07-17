1b55ac53813a50decaf29ccc30b7fc698adf2b6b
Ayesha khan <62783376+Ayesha-tech@users.noreply.github.com>
2020-07-18 00:17:27 +0530

    Create credit.sql

---
+++ credit.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2020 at 11:20 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(15) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `credit` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `email`, `credit`) VALUES
+('jia ','khan@gmail.com', '1000'),
+('akshay', 'aki@gmail.com', '1100'),
+('Akbar', 'akbar@gmail.com', '900'),
+('Aniket', 'aniket@gmail.com', '3600'),
+('shanker', 'shanker@gmail.com', '3500'),
+('mayuri', 'mayu@gmail.com', '100'),
+('Shan', 'Shan@gmail.com', '3000'),
+('sita', 'sita@gmail.com', '900'),
+('tejal', 'tejalv@gmail.com', '1200'),
+('vaisnvi', 'vaisnvi@gmail.com', '2000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
