46cfa923200d07a77c829c0bb0b26fc2d53637dd
Ayesha khan <62783376+Ayesha-tech@users.noreply.github.com>
2020-07-18 21:14:38 +0530

    Create dp.php

---
+++ dp.php
@@ -0,0 +1,10 @@
+<?php
+$server="sql310.epizy.com";
+$username="epiz_26289524";
+$password="Ml3a3VLih7";
+$dbname="epiz_26289524_comp";
+$con = mysqli_connect($_server,$_username,$_password,$_dbname);
+if(!$con) 
+{die("connection failed:"mysqli_connect_error));
+}
+>? 
